d6a1bfdc8cca4c1ee6ba0f95754129d88a3463b5
Lucelho Cristiano Vieira Da Silva <lucelhocristiano28@gmail.com>
2023-10-13 12:21:39 -0300

    starting project to do list

---
+++ Intermediate/todolist/src/main/java/br/com/lucelhosilva/todolist/user/UserController.java
@@ -0,0 +1,33 @@
+package br.com.lucelhosilva.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  /**
+   * String (texto)
+   * Integer (int) números inteiros
+   * Double (double) números 0.0000
+   * Float (float) números 0.000
+   * char (A C)
+   * Date (data)
+   * Void
+   */
+  @PostMapping("/")
+  public void create(@RequestBody UserModel userModel) {
+    System.out.println(userModel.name);
+    System.out.println(userModel.username);
+    System.out.println(userModel.password);
+  }
+  
+ }
