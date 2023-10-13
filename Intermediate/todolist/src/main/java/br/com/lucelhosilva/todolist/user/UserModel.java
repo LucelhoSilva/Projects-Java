7e85b3ec3ea1dd5af1a75fd7fbbedce298379997
Lucelho Cristiano Vieira Da Silva <lucelhocristiano28@gmail.com>
2023-10-13 16:50:43 -0300

    Add: Dependency in the project

--- Intermediate/todolist/src/main/java/br/com/lucelhosilva/todolist/user/UserModel.java
+++ Intermediate/todolist/src/main/java/br/com/lucelhosilva/todolist/user/UserModel.java
@@ -1,6 +1,10 @@
 package br.com.lucelhosilva.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
+  
   public String username;
   public String name;
   public String password;
